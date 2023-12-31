3cce36de80a326d0a48d7519b3bf7164eb25e7a0
Elcan <elcan@Elcans-MacBook-Air-2.local>
2023-12-31 22:18:39 +0400

    add other components to route between different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import FlashcardList from './components/FlashcardList';
+import HomePage from './components/HomePage';
+import ContactPage from './components/ContactPage';
+import OtherProjectsPage from './components/OtherProjectsPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Flashcards</h1>
-      <FlashcardList />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/flashcards">Flashcards</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Me</Link>
+            </li>
+            <li>
+              <Link to="/other-projects">Other Projects</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/flashcards" component={FlashcardList} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/other-projects" component={OtherProjectsPage} />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/OtherProjectsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OtherProjectsPage = () => {
+  return (
+    <div>
+      <h1>Other Projects</h1>
+    </div>
+  );
+};
+
+export default OtherProjectsPage;
